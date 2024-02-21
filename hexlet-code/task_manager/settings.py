3f825bab8a5cd1a8c627251f1f70cd25be348445
BogdanBarylo <barylob@gmail.com>
2024-02-21 15:20:47 +0300

    fix settings

--- hexlet-code/task_manager/settings.py
+++ hexlet-code/task_manager/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['webserver', '127.0.0.1']
+ALLOWED_HOSTS = ['webserver', '127.0.0.1', 'task-manager-site-tpi0.onrender.com']
 
 
 # Application definition
