0ff3ef2e0d236d3cd4a44d865f8ad105008117d0
BogdanBarylo <barylob@gmail.com>
2024-03-16 20:07:21 +0300

    fix bug with time

--- task_manager/users/tests.py
+++ task_manager/users/tests.py
@@ -4,6 +4,11 @@ from task_manager.users.models import ProjectUser
 from django.contrib.messages import get_messages
 
 
+USER_JAMES = {'first_name': 'James', 'last_name': 'May',
+              'username': 'CapitanSlow', 'password1': 'Huracan_321',
+              'password2': 'Huracan_321'}
+
+
 def get_message_txt(response):
     messages = list(get_messages(response.wsgi_request))
     return str(messages[0])
@@ -23,15 +28,12 @@ class CreateUserTestCase(TestCase):
 
     def test_create_user(self):
         c = Client()
-        response = c.post(reverse('create_user'),
-                          {'first_name': 'Richard', 'last_name': 'Hammond',
-                           'username': 'Mrcrash', 'password1': 'Mustang_123',
-                           'password2': 'Mustang_123'})
+        response = c.post(reverse('create_user'), USER_JAMES)
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, reverse('login'))
         self.assertEqual(get_message_txt(response),
                          'Пользователь успешно зарегистрирован')
-        created_user = ProjectUser.objects.get(username='Mrcrash')
+        created_user = ProjectUser.objects.get(username='CapitanSlow')
         self.assertIsNotNone(created_user)
 
 
@@ -43,10 +45,7 @@ class UpdateUserTestCase(TestCase):
         user_id = 1
         c.force_login(ProjectUser.objects.get(username='Jeza'))
         response = c.post(reverse('update_user', kwargs={'id': user_id}),
-                          {'first_name': 'James', 'last_name': 'May',
-                           'username': 'CapitanSlow',
-                           'password1': 'Huracan_321',
-                           'password2': 'Huracan_321'})
+                          USER_JAMES)
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, reverse('all_users'))
         self.assertEqual(get_message_txt(response),
@@ -60,10 +59,7 @@ class UpdateUserTestCase(TestCase):
         c = Client()
         user_id = 1
         response = c.post(reverse('update_user', kwargs={'id': user_id}),
-                          {'first_name': 'James', 'last_name': 'May',
-                           'username': 'CapitanSlow',
-                           'password1': 'Huracan_321',
-                           'password2': 'Huracan_321'})
+                          USER_JAMES)
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, reverse('login'))
         self.assertEqual(get_message_txt(response),
@@ -72,12 +68,9 @@ class UpdateUserTestCase(TestCase):
     def test_authorized_another_user_update(self):
         c = Client()
         user_id = 1
-        c.force_login(ProjectUser.objects.get(username='Stig'))
+        c.force_login(ProjectUser.objects.get(username='Mrcrash'))
         response = c.post(reverse('update_user', kwargs={'id': user_id}),
-                          {'first_name': 'James', 'last_name': 'May',
-                           'username': 'CapitanSlow',
-                           'password1': 'Huracan_321',
-                           'password2': 'Huracan_321'})
+                          USER_JAMES)
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, reverse('all_users'))
         self.assertEqual(get_message_txt(response),
@@ -109,7 +102,7 @@ class DeleteUserTestCase(TestCase):
     def test_authorized_another_user_delete(self):
         c = Client()
         user_id = 1
-        c.force_login(ProjectUser.objects.get(username='Stig'))
+        c.force_login(ProjectUser.objects.get(username='Mrcrash'))
         response = c.post(reverse('delete_user', kwargs={'id': user_id}))
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response.url, reverse('all_users'))
